484cfecde855dee05db6833d2e7a8542fd431019
Ozge Sezen Kirca <ozgesezen@gmail.com>
2022-12-07 14:05:14 +0300

    Hava Sicakligina Gore Ektinlik

---
+++ src/main/java/Q11_HavaSicakliginaGoreEtkinlikOnerme.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q11_HavaSicakliginaGoreEtkinlikOnerme {
+
+    public static void main(String[] args) {
+
+        int sicaklik;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sicaklik giriniz: ");
+        sicaklik = input.nextInt();
+
+        if (sicaklik<=5){
+            System.out.println("Kayak yapabilirsiniz");
+        } else if (sicaklik<=25) {
+            if (sicaklik>5 && sicaklik<=15){
+                System.out.println("Sinemaye gidebilirsin");
+            }
+            if (sicaklik>=10 && sicaklik<=25){
+                System.out.println("Piknige gidebilirsiniz");
+            }
+        }else {
+            System.out.println("Yuzmeye gidebilirsiniz");
+        }
+
+    }
+
+}
