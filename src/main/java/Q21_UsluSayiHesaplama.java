96f35b37bf0568397b12769cad463c0699413993
Ozge Sezen Kirca <ozgesezen@gmail.com>
2022-12-12 20:30:42 +0300

    Uslu Sayi Hesaplama

---
+++ src/main/java/Q21_UsluSayiHesaplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q21_UsluSayiHesaplama {
+
+    public static void main(String[] args) {
+
+        int a,b;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ussu alinacak sayi: ");
+        a= input.nextInt();
+        System.out.print("Us olacak sayi: ");
+        b= input.nextInt();
+        int total=1;
+
+        for (int i=1; i<=b; i++){
+            total*=a;
+        }
+        System.out.println("Sonuc: "+total);
+
+    }
+
+}
