1e3c9ab8a8be1a59d17881d37be8c98f8fd77f62
Ozge Sezen Kirca <ozgesezen@gmail.com>
2022-12-16 14:41:32 +0300

    Min Max Bulma

---
+++ src/main/java/Q27_MinMaxDeger.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q27_MinMaxDeger {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kac adet sayi gireceksiniz: ");
+        int a = input.nextInt();
+        int girilenSayi, maxSayi = 0, minSayi = 0;
+        for (int i = 1; i <= a; i++) {
+            System.out.print(i + ". Sayiyi giriniz: ");
+            girilenSayi = input.nextInt();
+            if (i == 1) {
+                maxSayi = girilenSayi;
+                minSayi = girilenSayi;
+            } else if (girilenSayi <= minSayi) {
+                minSayi = girilenSayi;
+            } else {
+                maxSayi = girilenSayi;
+            }
+        }
+        System.out.println("En buyuk sayi: " + maxSayi);
+        System.out.println("En kucuk sayi: " + minSayi);
+
+    }
+
+}
