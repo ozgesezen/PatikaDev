362b00927ae02408697ec1898053afd063bb6108
Ozge Sezen Kirca <ozgesezen@gmail.com>
2022-12-17 01:16:13 +0300

    Fibonacci Serisi

---
+++ src/main/java/Q31_FibonacciSerisi.java
@@ -0,0 +1,15 @@
+public class Q31_FibonacciSerisi {
+
+    public static void main(String[] args) {
+
+        int a=0 , b=1, toplam;
+        for (int i= 0; i<10;i++){
+            System.out.print(a+" ");
+            toplam=a+b;
+            a=b;
+            b=toplam;
+        }
+
+    }
+
+}
