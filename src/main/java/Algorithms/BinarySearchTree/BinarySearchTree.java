c6a7e8b3358aac162c07bfbf5c141f46a710cd8d
Ozge Sezen Kirca <ozgesezen@gmail.com>
2023-02-01 00:47:54 +0300

    01.02.23

---
+++ src/main/java/Algorithms/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package Algorithms.BinarySearchTree;
+
+import java.util.Arrays;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+
+    int[] arr = {7,5,1,8,3,6,0,9,4,2};
+
+     Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        //root degeri 5 dir saginda 7 degeri bulunur solunda 3 degeri bulunur
+    }
+
+}
