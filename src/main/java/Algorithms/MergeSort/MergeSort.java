4359a1c54ed9b69c1c9b7e9a5390f4b90bbee3b8
Ozge Sezen Kirca <ozgesezen@gmail.com>
2023-02-01 00:36:01 +0300

    01.02.23

--- src/main/java/Algorithms/MergeSort/MergeSort.java
+++ src/main/java/Algorithms/MergeSort/MergeSort.java
@@ -2,9 +2,61 @@ package Algorithms.MergeSort;
 
 public class MergeSort {
 
-    public static void mergeSort(int[] arr, int left, int middle, int right){
+    public static void merge(int[] arr, int left, int middle, int right) {
+
+        int low = middle - left + 1;
+        int high = right - middle;
+
+        int[] L = new int[low];
+        int[] R = new int[high];
+
+        int i = 0, j = 0;
+        for (i = 0; i < low; i++) {
+            L[i] = arr[left + i];
+        }
+        for (j = 0; j < high; j++) {
+            R[j] = arr[middle + 1 + j];
+        }
+
+        int k = left;
+        i = 0;
+        j = 0;
+
+        while (i < low && j < high) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < low) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < high) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
 
     }
 
+    public static void mergeSort(int arr[], int left, int right) {
+        int middle;
+        if (left < right) {
+            middle = (left + right) / 2;
 
+            mergeSort(arr, left, middle); // sol alt-array
+            mergeSort(arr, middle + 1, right); // sağ alt-array
+            merge(arr, left, middle, right); // sağ ve sol alt-arraylere merge işlemi yapılıyor
+
+        }
+    }
 }
+
+
+
--- src/main/java/Algorithms/MergeSort/MergeSortRunner.java
+++ src/main/java/Algorithms/MergeSort/MergeSortRunner.java
@@ -1,4 +1,18 @@
 package Algorithms.MergeSort;
 
+import java.util.Arrays;
+
 public class MergeSortRunner {
+
+    public static void main(String[] args) {
+
+        int[] arr = {16,21,11,8,12,22};
+        System.out.println("MergeSort Öncesi Array : ");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("MergeSort Sonrası Array : ");
+        MergeSort.mergeSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
 }
